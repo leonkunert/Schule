cfe777fcb9623d11f91995ba774f42dfabbf77c1
Administrator <leonkunert@gmail.com>
2013-04-05 14:59:54 +0200

    added Auftraeged

--- Java/05.04.13/Auftraeged.java
+++ Java/05.04.13/Auftraeged.java
@@ -1,14 +1,15 @@
 public class Auftraeged{
 
 	public static void main(String[] args) {
-		int anz = 5;//width and height
+		int anz = 20;//width and height
 		String str = "";
-		for(int x = 1; x<=anz; x++){
-			str = str+" "+x;
-			for(int y=1; y<=anz; y++){
-				str = 
+		for (int y = 1; y<=anz; y++) {//vertical
+			for(int x = 1; x<=anz; x++){//horizontal
+				str = str+" "+(x*y);
+				System.out.print(str);
+				str = "";
 			}
+			System.out.println();
 		}
-		System.out.println(str);
 	}
 }
